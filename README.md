e2842b2917921167cc9c70b40f32d0a7bc4df570
Martynas <martynas@atomgraph.com>
2019-08-13 10:55:10 +0200

    First commit - XSLTTransform function, README, LICENSE

---
+++ README.md
@@ -0,0 +1,36 @@
+# XSLTTransform
+
+XSLTTransform is a generic [AWS Lambda function](https://aws.amazon.com/lambda/) designed specifically for [Kinesis Data Firehose Data Transformation](https://docs.aws.amazon.com/firehose/latest/dev/data-transformation.html).
+
+XSLTTransform uses Saxon-HE 9.9 processor to perform an XSLT 3.0 transformation of input records to output records. XSLT 1.0 and 2.0 stylesheets should be backwards-compatible.
+Naturally, the input records have to be XML.
+
+Passing a map of stylesheet parameters is supported. Environment variables can be retrieved using `System.getenv()` and forwarded as stylesheet parameters.
+
+## Usage
+
+The XSLT stylesheet is user-defined and has to be supplied to the function. It can be done by extending the `XSLTTransform` base class and overriding its constructor with stylesheet and parameter map as arguments.
+
+The simplest way to embed a stylesheet is to put it under `/src/main/resources`, from where it can be read using `getResourceAsStream()`. That way the stylesheet is built into the function JAR itself. Alternatively, it should be possible to load it from S3.
+
+## Example
+
+The following example shows `/src/main/resources/custom.xsl` used as the XSLT stylesheet, as well as `my-param` stylesheet parameter initialized with the value of `MY_PARAM` environment variable.
+
+    package custom;
+
+    public class CustomXSLTTransform extends XSLTTransform
+    {
+        
+        public CustomXSLTTransform(String path, URI base) throws SaxonApiException
+        {
+            super(new StreamSource(CustomXSLTTransform.class.getResourceAsStream("/custom.xsl")), Collections.singletonMap(new QName("my-param"), new XdmAtomicValue(System.getenv("MY_PARAM"))));
+        }
+
+    }
+
+The function handler would be `custom.CustomXSLTTransform::transformRecords` in this case.
+
+## Build
+
+Package the function for upload to AWS Lambda by running `mvn package`.
---
+++ src/main/java/com/atomgraph/etl/aws/kinesis/transform/XSLTTransform.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2019 Martynas Jusevičius <martynas@atomgraph.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.atomgraph.etl.aws.kinesis.transform;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.KinesisAnalyticsInputPreprocessingResponse;
+import com.amazonaws.services.lambda.runtime.events.KinesisAnalyticsInputPreprocessingResponse.Record;
+import com.amazonaws.services.lambda.runtime.events.KinesisFirehoseEvent;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.xml.transform.stream.StreamSource;
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.QName;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.Serializer;
+import net.sf.saxon.s9api.XdmValue;
+import net.sf.saxon.s9api.Xslt30Transformer;
+import net.sf.saxon.s9api.XsltExecutable;
+
+/**
+ * AWS Lambda function that transforms Kinesis Firehose records using XSLT 3.0 processor.
+ * You need to extend this base class and to supply the stylesheet and its parameters to the subclass constructor.
+ * 
+ * @author Martynas Jusevičius <martynas@atomgraph.com>
+ */
+public class XSLTTransform
+{
+
+    private static final Processor PROCESSOR = new Processor(false);
+
+    private final XsltExecutable stylesheet;
+    private final Xslt30Transformer transformer;
+
+    public XSLTTransform(StreamSource stylesheet, Map<QName, XdmValue> params) throws SaxonApiException
+    {
+        this(PROCESSOR.newXsltCompiler().compile(stylesheet), params);
+    }
+    
+    public XSLTTransform(XsltExecutable stylesheet, Map<QName, XdmValue> params) throws SaxonApiException
+    {
+        this.stylesheet = stylesheet;
+        this.transformer = stylesheet.load30();
+        this.transformer.setStylesheetParameters(params);
+    }
+    
+    public KinesisAnalyticsInputPreprocessingResponse transformRecords(KinesisFirehoseEvent event, Context context) throws SaxonApiException
+    {
+        List<Record> transformed = event.getRecords().stream().map(inputRecord -> {
+            StreamSource input = new StreamSource(new ByteArrayInputStream(inputRecord.getData().array()));
+            
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            Serializer output = getProcessor().newSerializer(baos);
+            
+            Record record = new Record();
+            record.setRecordId(inputRecord.getRecordId());
+            try
+            {
+                getTransformer().transform(input, output);
+
+                record.setData(ByteBuffer.wrap(baos.toByteArray()));
+                record.setResult(KinesisAnalyticsInputPreprocessingResponse.Result.Ok);
+                return record;
+            }
+            catch (SaxonApiException ex)
+            {
+                log(ex);
+                record.setResult(KinesisAnalyticsInputPreprocessingResponse.Result.ProcessingFailed);
+                return record;
+            }
+        }).collect(Collectors.toList());
+        
+        KinesisAnalyticsInputPreprocessingResponse response = new KinesisAnalyticsInputPreprocessingResponse();
+        response.setRecords(transformed);
+        return response;
+    }
+    
+    public void log(Exception ex)
+    {
+        ex.printStackTrace();
+    }
+    
+    public Processor getProcessor()
+    {
+        return PROCESSOR;
+    }
+    
+    public XsltExecutable getStylesheet()
+    {
+        return stylesheet;
+    }
+    
+    public Xslt30Transformer getTransformer()
+    {
+        return transformer;
+    }
+    
+}
